87af2083cf9302d49d7419bb01879652f2dcbc7e
Heejin <hihj0914@gmail.com>
2025-03-28 23:12:18 +0900

    3-2 세션 완료

--- blogProject/posts/urls.py
+++ blogProject/posts/urls.py
@@ -20,4 +20,8 @@ from posts import views
 
 urlpatterns = [
     path("", views.posts, name = 'posts'),
+    path("post_list/", views.post_list, name = "post_list"),
+    path("post_detail/<int:post_id>/", views.post_detail, name = "post_detail"),
+    path("post_update/<int:id>/", views.post_update, name = "post_update"),
+    path("post_delete/<int:id>/", views.post_delete, name = "post_delete"),
 ]
