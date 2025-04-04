33512a011edf389e6438a7a1f8a4d2de11bb221b
Heejin <hihj0914@gmail.com>
2025-04-04 19:04:01 +0900

    블로그 플젝 완료

--- blogProject/posts/urls.py
+++ blogProject/posts/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for blogProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from posts import views
@@ -24,4 +8,9 @@ urlpatterns = [
     path("post_detail/<int:post_id>/", views.post_detail, name = "post_detail"),
     path("post_update/<int:id>/", views.post_update, name = "post_update"),
     path("post_delete/<int:id>/", views.post_delete, name = "post_delete"),
+
+    path('create_comment/<int:id>/', views.create_comment, name = 'create_comment'),
+    path('update_comment/<int:post_id>/<int:com_id>', views.update_comment, name = 'update_comment'),
+    path('delete_comment/<int:post_id>/<int:com_id>', views.delete_comment, name = 'delete_comment'),
+
 ]
--- blogProject/posts/views.py
+++ blogProject/posts/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from blog.models import Post
-from blog.forms import PostModelForm
+from blog.models import Post, Comment
+from blog.forms import PostModelForm, CommentForm
+from django.core.paginator import Paginator
 
 def posts(request):
     return render(request, "posts.html")
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
+    my_paginator = Paginator(posts, 5)
+    page_num = request.GET.get('page')
+    posts = my_paginator.get_page(page_num)
     return render(request, "post_list.html", {"posts" : posts})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk = post_id)
-    return render(request, "post_detail.html", {"post" : post})
+    comment_form = CommentForm()
+    context = {
+        'post' : post,
+        'comment_form' : comment_form
+    }
+    return render(request, "post_detail.html", context)
 
 def post_update(request, id):
     post = get_object_or_404(Post, pk = id)
 
-    if request.method == 'POST':
-        form = PostModelForm(request.POST, instance = post)
+    if request.method == 'POST' or request.method == 'FILES':
+        form = PostModelForm(request.POST, request.FILES, instance = post)
 
         if form.is_valid():
             form.save()
@@ -31,3 +40,30 @@ def post_delete(request, id):
     post = Post.objects.get(pk = id)
     post.delete()
     return redirect('post_list')
+
+def create_comment(request, id):
+    filled_form = CommentForm(request.POST)
+    if filled_form.is_valid():
+        finished_form = filled_form.save(commit = False)
+        finished_form.article = get_object_or_404(Post, pk = id)
+        finished_form.author = request.user
+        finished_form.save()
+    return redirect('post_detail', id)
+
+def update_comment(request, post_id, com_id):
+    comment = Comment.objects.get(id = com_id)
+
+    if request.method == 'POST':  # 사용자가 수정 후 POST 요청을 보냈을 때
+        updated_form = CommentForm(request.POST, instance = comment)
+        if updated_form.is_valid():
+            updated_form.save()
+            return redirect('post_detail', post_id)
+    else:   # Get 요청일 때
+        comment_form = CommentForm(instance = comment)
+        context = {'comment_form' : comment_form}
+        return render(request, 'comment_update.html', context)
+    
+def delete_comment(request, post_id, com_id):
+    comment = Comment.objects.get(id = com_id)
+    comment.delete()
+    return redirect('post_detail', post_id)
